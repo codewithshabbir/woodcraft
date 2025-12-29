7b6747e6152f4e67f824f5e5680d9b08e7f9eb54
codewithshabbir <muhammadshabbir45890@gmail.com>
2025-12-29 21:14:34 +0500

    type error fixed

--- types/auth.ts
+++ types/auth.ts
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import { Control, FieldValues, Path } from "react-hook-form";
 
+export interface LayoutProps {
+  children: ReactNode;
+}
+
 export interface PrimaryFormFieldProps<T extends FieldValues> {
   name: Path<T>;
   label: string;
